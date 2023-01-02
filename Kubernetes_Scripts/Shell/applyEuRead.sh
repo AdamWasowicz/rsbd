371ff397bd7d2a2ad2f9be88be423bd4750d5b46
Adam Wąsowicz <adam.p.wasowicz@gmail.com>
2023-01-02 19:04:15 +0100

    EU deployment

---
+++ Kubernetes_Scripts/Shell/applyEuRead.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Variables
+PURPLE='\033[0;95m'
+COLOR_OFF='\033[0m'
+GREEN='\033[0;92m'
+CYAN='\033[0;96m'  
+
+echo -e "${GREEN}[APPLY EU] - BEGIN${COLOR_OFF}"
+
+kubectl apply -f ../Yml/eu-read-volume.yml
+kubectl apply -f ../Yml/eu-read-deployment.yml
+
+echo -e "${GREEN}[APPLY EU] - END${COLOR_OFF}"
