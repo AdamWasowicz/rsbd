63f0c32a0b91cfb0d696a35ee091c1d956203e98
Adam Wąsowicz <adam.p.wasowicz@gmail.com>
2023-01-05 22:02:15 +0100

    FE/404 page

--- RSBD_FE/src/pages/Error404/Error404.tsx
+++ RSBD_FE/src/pages/Error404/Error404.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import './style.scss';
+import ErrorMessage from '../../components/ErrorMessage';
 
 const Error404: React.FunctionComponent = () => {
 
     return (
-        <div>
-            Error404
+        <div className='Error'>
+            <ErrorMessage
+                title='404'
+                textContent='The page that you are looking for does not exist, please try using navigation to find your way around the app. Have a nice day.'
+            />
         </div>
     )
 }
